95e0cbf62bd784c468867d2265f42b3302fb0ce9
Marc Remolt <marc.remolt@gmail.com>
2022-01-14 12:48:20 +0100

    Basic angular & jest setup

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['src/app/**/*.ts', '!src/app/**/*.spec.ts', '!src/app/**/*.module.ts'],
+};
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,26 @@
-import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { render, screen } from '@testing-library/angular';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+  let component: AppComponent;
+
+  beforeEach(() => {
+    component = new AppComponent();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it(`should have as title 'ng-test-setup'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-test-setup');
+    expect(component.title).toEqual('ng-test-setup');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ng-test-setup app is running!');
+  it('should render title', async () => {
+    await render(AppComponent, { imports: [RouterTestingModule] });
+
+    expect(screen.getByText('ng-test-setup app is running!'));
   });
 });
